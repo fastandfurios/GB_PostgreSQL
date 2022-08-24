301f8d7078c0d31715c1d0505b51da687778922d
Дмитрий <semu-dima@yandex.ru>
2022-08-24 07:57:06 +0300

    Update script_2.sql

--- Lesson_6/script_2.sql
+++ Lesson_6/script_2.sql
@@ -1,23 +1,16 @@
-CREATE OR REPLACE PROCEDURE main_photo_id_cleanup()
-LANGUAGE PLPGSQL AS
+CREATE OR REPLACE PROCEDURE check_owner_main_photo()
+LANGUAGE plpgsql AS
 $$
-DECLARE photo_profiles_row RECORD;
-BEGIN
-    FOR photo_profiles_row IN
-        SELECT owner_id,
-                main_photo_id
-        FROM profiles
-            LEFT JOIN photo p on p.id = profiles.main_photo_id
-        ORDER BY owner_id
-    LOOP
-        UPDATE profiles SET main_photo_id = NULL WHERE user_id = photo_profiles_row.owner_id;
-    END LOOP;
-    COMMIT;
-END;
-$$;
-
-ALTER TABLE profiles ALTER COLUMN main_photo_id DROP NOT NULL;
-
-CALL main_photo_id_cleanup();
-
-SELECT main_photo_id FROM profiles;
+    DECLARE real_owner RECORD;
+    BEGIN
+       FOR real_owner IN
+           SELECT profiles.user_id
+                FROM profiles
+                    JOIN photo p on p.id = profiles.main_photo_id
+           WHERE p.owner_id != profiles.user_id
+        LOOP
+           UPDATE profiles SET main_photo_id = NULL WHERE user_id = real_owner.user_id;
+        END LOOP;
+       COMMIT;
+    END;
+$$

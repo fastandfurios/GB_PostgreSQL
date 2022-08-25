489f5b5fd2b2b92f06c5b55d5d8ee977d5783fa1
Дмитрий <semu-dima@yandex.ru>
2022-08-25 23:19:19 +0300

    Update script_6.sql

--- final_project/script_6.sql
+++ final_project/script_6.sql
@@ -1,13 +1,13 @@
 -- запрос выбирает владельцев групп и только с фото профиля
-SELECT
+SELECT DISTINCT
     first_name,
     last_name,
     g.name,
     p.url
     FROM users
-        LEFT JOIN groups g ON users.id = g.creator_id
+        JOIN groups g ON users.id = g.creator_id
         LEFT JOIN pictures p ON users.id = p.owner_id
-    WHERE p.url NOTNULL AND g.name NOTNULL;
+    WHERE name NOTNULL AND url NOTNULL;
 
 -- запрос выбирает 5 самых просматриваемых видео исполнителей песен
 SELECT DISTINCT
@@ -17,7 +17,7 @@ SELECT DISTINCT
     v.size,
     views
     FROM music
-        LEFT JOIN users u ON music.musician_id = u.id
+        JOIN users u ON music.musician_id = u.id
         LEFT JOIN video v ON u.id = v.owner_id
     WHERE v.size NOTNULL
     ORDER BY views DESC LIMIT 5;
